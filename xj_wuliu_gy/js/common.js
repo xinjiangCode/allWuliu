981e0169b7e87f76a5d78ddd701663987d56c42d
luchenfei <luchenfei@gooday123.com>
2018-08-12 20:31:13 +0800

    111

--- xj_wuliu_gy/js/common.js
+++ xj_wuliu_gy/js/common.js
@@ -1,5 +1,5 @@
 //var pubIP = 'http://192.168.1.78:7777/service/';
-// var pubIP = 'http://192.168.1.199:7777/service/';
+var pubIP = 'http://192.168.1.199:7777/service/';
 //  var pubIP = 'http://wl.api.xjv56.com/service/';
 
 
