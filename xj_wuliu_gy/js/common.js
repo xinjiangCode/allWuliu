cba12053d823250c80c3158f01508cd490d954ce
huzhengfeng <huzhengfeng@gooday123.com>
2018-08-14 20:29:14 +0800

    22

--- xj_wuliu_gy/js/common.js
+++ xj_wuliu_gy/js/common.js
@@ -1,5 +1,5 @@
-var pubIP = 'http://192.168.1.218:7777/service/';
-//var pubIP = 'http://192.168.1.199:7777/service/';
+//var pubIP = 'http://192.168.1.218:7777/service/';
+var pubIP = 'http://192.168.1.199:7777/service/';
 // var pubIP = 'http://192.168.1.218:7777/service/';
 //  var pubIP = 'http://wl.api.xjv56.com/service/';
 
