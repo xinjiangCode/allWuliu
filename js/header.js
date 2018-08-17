bb05b666762995900a1af4f5273f050004c2051e
huzhengfeng <huzhengfeng@gooday123.com>
2018-08-17 09:47:50 +0800

    22

--- js/header.js
+++ js/header.js
@@ -1,8 +1,8 @@
 // 公共头
 var adct = document.getElementsByTagName('title')[0].getAttribute('adct');
 //公共IP
-// var headerip = 'http://192.168.1.219:7777/service/';
-var headerip = 'http://192.168.1.199:7777/service/';
+var headerip = 'http://192.168.1.219:7777/service/';
+//var headerip = 'http://192.168.1.199:7777/service/';
 //var headerip = 'http://wl.api.xjv56.com/service/';
 
 //获取地址栏参数，name:参数名称
