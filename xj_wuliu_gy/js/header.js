c462a4f5dc9135a0415705a06dd6af783fcd1cc6
luchenfei <luchenfei@gooday123.com>
2018-08-23 12:21:11 +0800

    111

--- xj_wuliu_gy/js/header.js
+++ xj_wuliu_gy/js/header.js
@@ -3,7 +3,8 @@ var adct = document.getElementsByTagName('title')[0].getAttribute('adct');
 //公共IP
 // var headerip = 'http://192.168.1.199:7777/service/';
 // var headerip = 'http://192.168.1.218:7777/service/';
-var headerip = 'http://wl.api.xjv56.com/service/';
+var headerip = 'http://api.hdlsuper.com/service/';
+// var headerip = 'http://wl.api.xjv56.com/service/';
 
 //获取地址栏参数，name:参数名称
 function getUrlParms(name){
