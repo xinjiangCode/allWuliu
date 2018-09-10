1e8deaa0fbcc2780e28ba8a03a2429be6cc42905
huzhengfeng <huzhengfeng@gooday123.com>
2018-09-10 19:12:24 +0800

    22

--- xj_wuliu_gy/js/header.js
+++ xj_wuliu_gy/js/header.js
@@ -1,9 +1,9 @@
 // 公共头
 var adct = document.getElementsByTagName('title')[0].getAttribute('adct');
 //公共IP
-// var headerip = 'http://192.168.1.199:7777/service/';
+var headerip = 'http://192.168.1.199:7777/service/';
 // var headerip = 'http://192.167.1.185:7777/service/';
-var headerip = 'http://api.hdlsuper.com/service/';
+//var headerip = 'http://api.hdlsuper.com/service/';
 
 
 // var headerip = 'http://wl.api.xjv56.com/service/';
