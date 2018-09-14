361dadc46b2316d0075ef3bc39348b17be0f216c
18838514879 <18838514879@163.com>
2018-09-14 16:36:33 +0800

    111

--- js/header.js
+++ js/header.js
@@ -5,7 +5,7 @@ var adct = document.getElementsByTagName('title')[0].getAttribute('adct');
 var headerip = 'http://192.168.1.199:7777/service/';
 // var headerip = 'http://api.hdlsuper.com/service/';
 // var headerip = 'http://api.test.hdlsuper.com/service/';
-// var headerip = 'http://wl.api.xjv56.com/service/';
+// var headerip = 'http://wl.api.xjv56.com/service/'; //不需要了
 //
 //获取地址栏参数，name:参数名称
 function getUrlParms(name){
