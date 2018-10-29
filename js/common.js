54f94edb7552042417d8fa61125c11e9c127c8dc
18838514879 <18838514879@163.com>
2018-10-29 19:33:22 +0800

    111

--- js/common.js
+++ js/common.js
@@ -1,6 +1,6 @@
 
-var pubIP = 'http://192.168.1.68:7777/service/';
-// var pubIP = 'http://192.168.1.199:7777/service/';
+// var pubIP = 'http://192.168.1.68:7777/service/';
+var pubIP = 'http://192.168.1.199:7777/service/';
 // var pubIP = 'http://api.hdlsuper.com/service/';
 // var pubIP = 'http://wl.api.xjv56.com/service/';
 // var pubIP = 'http://api.test.hdlsuper.com/service/';
